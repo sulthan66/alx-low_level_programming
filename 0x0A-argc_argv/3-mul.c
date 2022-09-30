5ed2d85a101f5a86c08b0d7ac037ff4fa41aaee0
Sultan Tiamiyu <60345346+sulthan66@users.noreply.github.com>
2022-09-30 22:45:24 +0100

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - prints all arguments it recieves
+ * main - multiplies two numbers
  * @argc: number of arguments passed to the function
  * @argv: argument vector of pointers to strings
  *
- * Return: always 0
+ * Return: 0 if no errors, else 1
  */
 int main(int argc, char *argv[])
 {
-	int i;
+	int a, b, c;
 
-	for (i = 0; i < argc; i++)
+	if (argc != 3)
 	{
-		printf("%s\n", argv[i]);
+		puts("Error");
+		return (1);
 	}
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	c = a * b;
+	printf("%d\n", c);
 	return (0);
 }
